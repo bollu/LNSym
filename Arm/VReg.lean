5a1cc712119dd1a0435075dd98dc041561891d82
Siddharth Bhat <siddu.druid@gmail.com>
2024-07-03 13:24:13 +0100

    chore: think about interface neeed for abstraction

---
+++ Arm/VReg.lean
@@ -0,0 +1,1 @@
+import Arm.VReg.Real2Abstract
---
+++ Arm/VReg/Real2Abstract.lean
@@ -0,0 +1,9 @@
+import Arm.Cfg.Cfg
+
+/-
+- Create a correspondence between (BitVec 5) and Nat for the index set.
+- Question: how do we map the semantics that's written in terms of the old memory model?
+- Question: I guess we have a mapping from virtual to real registers, and we show that
+    the virtual register matches the real register values.
+-/
+
